94c6e4a7f945cc10e56db3e4df04f085151af52a
Riantavares <rian.tavares@icloud.com>
2017-08-08 01:10:03 -0300

    first commit

---
+++ public/js/controllers/foto-controller.js
@@ -0,0 +1,22 @@
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
+
+  $scope.fotos = [];
+
+  $http.get('v1/fotos')
+  .success(function(data) { /* valor dentro da function corresponde a resposta da rota*/
+    $scope.fotos = data;
+  })
+  .error(function(erro) {
+    console.log(erro);
+  });
+
+
+/*
+  var promise = $http.get('v1/fotos');
+  promise.then(function(retorno){
+    $scope.fotos = retorno.data;
+  }).catch(function(error){
+    console.log(error);
+  });
+*/
+});
