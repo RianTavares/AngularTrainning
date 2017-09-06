7524237f4763121b27e45ffbfa97c1f0e47c4b80
Riantavares <rian.tavares@icloud.com>
2017-09-06 01:53:56 -0300

    creating a Two-way data binding filter field

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,6 +1,8 @@
 angular.module('alurapic').controller('FotosController', function($scope, $http) {
 
   $scope.fotos = [];
+  $scope.filtro = '';
+
 
   $http.get('v1/fotos')
   .success(function(data) { /* valor dentro da function corresponde a resposta da rota*/
